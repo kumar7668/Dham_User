d4c1cc5191e12baceb97a3518f17fc197d6848c8
Sonukumar <sonu.mandal@pinogy.com>
2024-08-27 22:23:45 +0530

    api added for 1st carousel template

--- dham_user/views.py
+++ dham_user/views.py
@@ -3,15 +3,15 @@ from dod_api.api_hooks import get_api_data
 
 # Create your views here.
 def home(request):
-    # # Call the utility function with the appropriate key
-    # city_data = get_api_data('get_all_city')
-    # # Pass the data to the template context
-    # context = {
-    #     'city_data': city_data if city_data else []  # Ensure it's not None
-    # }
-    # return render(request, 'base.html', context)
+    # Call the utility function with the appropriate key
+    city_data = get_api_data('get_all_city')
+    # Pass the data to the template context
+    context = {
+        'city_data': city_data if city_data else []  # Ensure it's not None
+    }
+    return render(request, 'base.html', context)
 
-    return render(request, 'base.html')
+    # return render(request, 'base.html')
 
 
 
